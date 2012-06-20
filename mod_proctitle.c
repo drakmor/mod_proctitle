b0704480e5b288b6e78a5633d210e650e4b59abb
John Keith Hohm <john@hohm.net>
2012-06-20 15:08:10 -0500

    Fix warnings (probably not behavior-impacting):
      * Signature of child_init hook function was wrong, but arguments were not used and returning int instead of void does not matter.
    
      * Definition of translate_name hook function requires http_request.h inclusion.
    
      * Example handler hook function was unused.

--- mod_proctitle.c
+++ mod_proctitle.c
@@ -21,25 +21,14 @@
 */
 #include "httpd.h"
 #include "http_config.h"
+#include "http_request.h"
 #include "http_protocol.h"
 #include "ap_config.h"
 extern char *ap_server_argv0;
 char proctitle_buf[128];
-/* The sample content handler */
-static int proctitle_handler(request_rec *r)
-{
-   if (strcmp(r->handler, "proctitle")) {
-       return DECLINED;
-   }
-   r->content_type = "text/html";
-   if (!r->header_only)
-       ap_rputs("The sample page from mod_proctitle.c\n", r);
-   return OK;
-}
-static int proctitle_child_init(request_rec *r)
+static void proctitle_child_init(apr_pool_t *pchild, server_rec *s)
 {
        strncpy(ap_server_argv0,"httpd: init-child",64);
-       return DECLINED;
 }
 static int proctitle_clear(request_rec *r)
 {
@@ -57,7 +46,6 @@ static int proctitle_ft(request_rec *r)
 }
 static void proctitle_register_hooks(apr_pool_t *p)
 {
-   /* ap_hook_handler(proctitle_handler, NULL, NULL, APR_HOOK_MIDDLE); */
    ap_hook_child_init(proctitle_child_init, NULL, NULL, APR_HOOK_FIRST);
    ap_hook_translate_name(proctitle_ft,NULL,NULL, APR_HOOK_FIRST);
    ap_hook_log_transaction(proctitle_clear,NULL,NULL, APR_HOOK_LAST);
